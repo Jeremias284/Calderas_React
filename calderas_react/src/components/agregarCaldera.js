1b7cb0574faf478a3c186d8f767a0c9759cd06b7
Jeremias Calzada <calzadajeremias246@gmail.com>
2021-10-02 12:22:16 -0300

    Corrigiendo errores

--- calderas_react/src/components/agregarCaldera.js
+++ calderas_react/src/components/agregarCaldera.js
@@ -1,30 +1,43 @@
-import React from "react";
-import PropTypes from 'prop-types'
+import React from 'react'
+import {useState} from 'react';
 
-const Boton = ({color1, color2, text1, text2, onClick}) => {
-    return(
-    <div>
-        <button onClick={onClick} 
-            style= {{backgroundColor: color1}} 
-            className='btn'>{text1} 
-        </button>
-        <button onClick={onClick} 
-            style= {{backgroundColor: color2}} 
-            className='btn'>{text2} 
-        </button>
-    </div>
-    
-    ) 
-    
+ const AgregarCaldera = ({onAdd}) => {
+    const [nombre, setNombre] = useState('')
+    const [caldera, setCaldera] = useState('')
+    const [activo, setActivo] = useState(false)
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+        if(!nombre){
+            alert('introduzca datos');
+            return;
+        }
+        onAdd({nombre, caldera, activo});
+        setNombre('');
+        setCaldera('');
+        setActivo(false);
+    }
+
+    return (
+        <form className= 'add-form' onSubmit = { onSubmit}>
+            <div className= 'form-control'>
+                <label>Tecnicos</label>
+                <input type= 'text' placeholder='ingrese el nombre del tecnico' value={nombre} onChange= {(e) => setNombre(e.target.value)} />
+            </div>
+            <div className= 'form-control '>
+                <label>Caldera</label>
+                <input type= 'text' placeholder='ingrese el tipo de caldera' value={caldera} onChange= {(e) => setCaldera(e.target.value)} />
+            </div>
+            <div className= 'form-control form-control-check'>
+                <label>Activo</label>
+                <input type= 'checkbox' checked={activo} value={activo} onChange= {(e) => setActivo(e.currentTarget.checked)} />
+            </div>
+
+            <input type= 'submit' value= 'Guardar Caldera' className= 'btn btn-block' />
+        </form>
+    )
 }
 
-Boton.defaultProps = {
-    color: 'steelblue',
-}
 
-Boton.propTypes = {
-    text: PropTypes.string,
-    color: PropTypes.string, 
-}
-
-export default Boton
+export default AgregarCaldera;
